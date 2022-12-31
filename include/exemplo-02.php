de1c4f42f6907c5d4edd76f099cbc2d9d81e6a4c
Daniel Franco <82846096+Franccodan@users.noreply.github.com>
2022-12-31 10:30:11 -0300

    Add files via upload

---
+++ include/exemplo-02.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+//include "exemplo-01.php";
+
+//Função identica a include, o único porém está na condição de que o arquivo tem de estar funcionando corretamente
+require "exemplo-01.php";
+
+$resultado = somar(10, 20);
+
+echo $resultado;
+
+?>
