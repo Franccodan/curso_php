8b76913e2c4fecd88cabb2d765ae6d1d88f03845
Daniel Franco <82846096+Franccodan@users.noreply.github.com>
2023-01-03 22:11:31 -0300

    Add files via upload

---
+++ Switch case/exemplo-01.php
@@ -0,0 +1,41 @@
+<?php 
+
+$diaDaSemana = "8"; //date("w");
+
+
+//Estrutura do switch
+switch ($diaDaSemana) {
+
+	case 1:
+	echo "Domingo";
+	break;
+
+	case 2:
+	echo "Segunda";
+	break;
+
+	case 3:
+	echo "Terça";
+	break;
+
+	case 4:
+	echo "Quarta";
+	break;
+
+	case 5:
+	echo "Quinta";
+	break;
+
+	case 6:
+	echo "Sexta";
+	break;
+
+	default:
+	echo "Data inválida";
+	break;
+
+
+}
+
+
+?>
